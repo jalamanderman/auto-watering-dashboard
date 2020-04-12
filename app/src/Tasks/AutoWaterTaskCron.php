d5ff7e3a78bbcf9ec364a795ba3e615729dcd1df
Jack <jack_allan1@hotmail.com>
2020-04-13 09:21:48 +1200

    cron timing

--- app/src/Tasks/AutoWaterTaskCron.php
+++ app/src/Tasks/AutoWaterTaskCron.php
@@ -1,28 +1,28 @@
 <?php
 
-namespace {
-    use SilverStripe\CronTask\Interfaces\CronTask;
+use SilverStripe\CronTask\Interfaces\CronTask;
 
-    class AutoWaterTaskCron implements CronTask {
+class AutoWaterTaskCron implements CronTask {
 
-        protected $title = 'Cron to run notices task which compiles the JSON data for the notices map';
-        protected $enabled = true;
+    protected $title = 'Cron to run notices task which compiles the JSON data for the notices map';
+    protected $enabled = true;
 
-        public function getSchedule() {
+    public function getSchedule() {
 //            return "0 22 */2 * * "; //run every 2 days at 10pm
-            return "*/2 * * * *"; //run every 2 minutes
+        return "* * * * *"; //run every 2 minutes
 
-        }
+    }
 
-        public function process(){
+    public function process() {
+        $AutoWaterTask = new AutoWaterTask();
+        $AutoWaterTask->run($request = null);
 
-            $AutoWaterTask = new AutoWaterTask();
-            $dashboard = Dashboard::get()->first();
+        $dashboard = Dashboard::get()->first();
 
-            if ($dashboard->AutoWatering == 'ON') {
-                $AutoWaterTask->run($request=null);
-            }
+        if ($dashboard->AutoWatering == 'ON') {
+            $AutoWaterTask->run($request = null);
         }
     }
 }
+
 
